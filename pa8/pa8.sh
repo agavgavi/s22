c32a0cf2433985557e70a66f823e48dd233fc068
Andrew Gavgavian <andrewgee7@gmail.com>
2022-05-24 14:17:46 -0700

    added pa8 for testing

---
+++ pa8/pa8.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# s22
+# usage: pa8.sh
+# (run within your pa8 directory to test your code)
+
+SRCDIR=https://raw.githubusercontent.com/agavgavi/s22/master/pa8
+
+EXE_ALL=( pa8-func-check.sh pa8-unit-check.sh pa8-make-check.sh )
+EXE_RANGE=$((${#EXE_ALL[*]} - 1))
+echo $EXE_RANGE
+for i in $(seq 0 $EXE_RANGE); do
+  curl $SRCDIR/${EXE_ALL[i]} > ${EXE_ALL[i]}
+  chmod +x ${EXE_ALL[i]}
+  ./${EXE_ALL[i]}
+  rm -f ${EXE_ALL[i]}
+done
